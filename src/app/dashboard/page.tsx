bebfb8f5018445912a320f5d44e94e045fc139ef
Oliver Müller <oliverm1604@gmail.com>
2023-12-28 17:16:46 +0100

    DB Structure

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from "next/navigation"
 import { db } from "../db"
 import Dashboard from "@/components/Dasboard"
+import { title } from "process"
 
 const Page = async () => {
     const {getUser} = getKindeServerSession()
@@ -12,11 +13,31 @@ const Page = async () => {
     const dbUser = await db.user.findFirst({
         where: {
             id: user.id
+        },
+        include:{
+            forms: true
         }
     })
 
-    if(!dbUser) redirect('/auth-callback?origin=dashboard')
+    const dBFormsFromUser = await db.form.findMany({
+        where:{
+            ownerId: dbUser?.id
+        },
+    })
 
+    const dbCategoryFromForm = await db.category.findMany({
+        where:{
+            formId: dBFormsFromUser.id
+        },
+        select: {
+            questions: {
+                select: {
+                    title: true
+            }
+        }
+    }})
+    if(!dbUser) redirect('/auth-callback?origin=dashboard')
+    console.log(dbCategoryFromForm)
     return(
     <div>
         <Dashboard/>
